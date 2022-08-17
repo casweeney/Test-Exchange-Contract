70082a5d8ccaf077749cee08661697c4ce818a05
CodingCas <casweeno2000@gmail.com>
2022-08-17 19:29:26 +0100

    Edited

--- contracts/Swap.sol
+++ contracts/Swap.sol
@@ -13,7 +13,7 @@ contract Swap {
     }
 
     //swap function
-    function swapToken(address trader1, address trader2, uint256 amount1, uint256 amount2) external {
+    function tokenSwap(address trader1, address trader2, uint256 amount1, uint256 amount2) external {
         require(token1.allowance(trader1, address(this)) >= amount1, "Allowance for token one too low");
         require(token2.allowance(trader2, address(this)) >= amount2, "Allowance for token two too low");
         transferToken(token1, trader1, trader2, amount1);
